41eb9779561b9a2d61d7aa6023fc7f33f10ee6df
Yashasewi <yashasewisingh2668@gmail.com>
2024-01-27 00:38:44 +0530

    added normal js for the clock

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,52 @@
-import dayjs from 'dayjs';
-import { useState, useEffect } from 'react';
+import dayjs from "dayjs";
+import { useState, useEffect } from "react";
 
 function App() {
-  const [minute, setMinute] = useState(dayjs().minute());
-  const [second, setSecond] = useState(dayjs().second());
-  const [hour, setHour] = useState(dayjs().hour());
+    const [minute, setMinute] = useState(dayjs().minute());
+    const [second, setSecond] = useState(dayjs().second());
+    const [hour, setHour] = useState(dayjs().hour());
 
-  useEffect(() => {
-    const intervalId = setInterval(() => {
-      setMinute(dayjs().minute());
-      setSecond(dayjs().second());
-      setHour(dayjs().hour());
-    }, 1000);
+    useEffect(() => {
+        const intervalId = setInterval(() => {
+            setMinute(dayjs().minute());
+            setSecond(dayjs().second());
+            setHour(dayjs().hour());
+        }, 1000);
 
-    return () => clearInterval(intervalId);
-  }, []); 
+        return () => clearInterval(intervalId);
+    }, []);
 
-  return (
-    <div className="flex justify-center items-center w-screen h-screen bg-black">
-      <div className="flex text-[20rem] font-bold text-white select-none">
-        <h1>{hour}</h1>
-        <span>:</span>
-        <h1>{minute}</h1>
-        <span>:</span>
-        <h1>{second}</h1>
-      </div>
-    </div>
-  );
+    function AddZero(value) {
+        let last = ~~(value % 10);
+        let first = ~~(value / 10);
+        // also can use math.trunc()
+        // console.log("f :" + first + ~~last);
+        return first.toString() + last;
+    }
+
+    return (
+        <div className="flex justify-center items-center w-screen h-screen bg-black">
+            <div className="flex text-[20rem] overflow-hidden green select-none  h-[30vh] max-h-[30vh] justify-center items-center m-1 p-1 ">
+                <div className="grad h-full w-full absolute pointer-events-none max-h-[29vh]"></div>
+                <div className="h-full w-full absolute pointer-events-none shadow"></div>
+                <h1>{AddZero(hour) % 12 === 0 ? "12" : AddZero(hour) % 12}</h1>
+                <span>:</span>
+                <h1>{AddZero(minute)}</h1>
+                <span>:</span>
+                <div className=" flex flex-col h-[30vh]">
+                    <h1 className="h-full  flex items-center ">
+                        {AddZero(second)}
+                    </h1>
+                    <h1 className="h-full  flex items-center">
+                        {AddZero(second + 1)}
+                    </h1>
+                </div>
+                <h1 className="text-5xl h-full flex items-end mb-4">
+                    {hour > 12 ? "PM" : "AM"}
+                </h1>
+            </div>
+        </div>
+    );
 }
 
 export default App;
-
-
-
