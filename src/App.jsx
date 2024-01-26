927f93548b4c59ac45487fd1d99b2c586bff2bda
Yashasewi <yashasewisingh2668@gmail.com>
2024-01-26 21:34:22 +0530

    a normal clock

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,35 @@
-//import { useState } from 'react'
-//import reactLogo from './assets/react.svg'
-//import viteLogo from '/vite.svg'
-import './App.css'
+import dayjs from 'dayjs';
+import { useState, useEffect } from 'react';
 
 function App() {
-  // const [count, setCount] = useState(0)
+  const [minute, setMinute] = useState(dayjs().minute());
+  const [second, setSecond] = useState(dayjs().second());
+  const [hour, setHour] = useState(dayjs().hour());
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setMinute(dayjs().minute());
+      setSecond(dayjs().second());
+      setHour(dayjs().hour());
+    }, 1000);
+
+    return () => clearInterval(intervalId);
+  }, []); 
 
   return (
-    <>
-      <h1 className="text-3xl font-bold underline  text-red-500">
-        Hello world!
-      </h1>
-    </>
-  )
+    <div className="flex justify-center items-center w-screen h-screen bg-black">
+      <div className="flex text-[20rem] font-bold text-white select-none">
+        <h1>{hour}</h1>
+        <span>:</span>
+        <h1>{minute}</h1>
+        <span>:</span>
+        <h1>{second}</h1>
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
+
+
+
